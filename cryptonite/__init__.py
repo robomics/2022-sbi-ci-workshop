80ded933738770cf1920461a5427669da676816e
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-10-10 18:46:22 +0200

    Bugfix

--- cryptonite/__init__.py
+++ cryptonite/__init__.py
@@ -4,10 +4,14 @@
 
 from __future__ import absolute_import
 
-from setuptools_scm import get_version
+from importlib.metadata import version, PackageNotFoundError
 
 import cryptonite.decrypt
 import cryptonite.encrypt
 import cryptonite.utils
 
-__version__ = get_version()
+try:
+    __version__ = version("cryptonite")
+except PackageNotFoundError:
+    # package is not installed
+    pass
