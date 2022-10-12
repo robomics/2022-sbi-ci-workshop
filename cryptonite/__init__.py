fbf0de50f3becd4954d1ea59b648e278184ef35d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-10-12 17:47:43 +0200

    Remove cryptonite code

--- cryptonite/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
-#
-# SPDX-License-Identifier: MIT
-
-from importlib.metadata import version
-
-__version__ = version("2022-sbi-ci-workflow-cryptonite")
